3c566a509055ca88137ce68a9578fa3eb8f25d5f
t-rryyyoooo <rryyyoooo@i.softbank.jp>
2020-10-07 09:59:40 +0000

    R

--- UNet_no_pad/preprocessing.py
+++ UNet_no_pad/preprocessing.py
@@ -57,7 +57,8 @@ class GetArrayFromImage(object):
         label_array = sitk.GetArrayFromImage(label).astype(int)
 
         if image.GetDimension() != 4:
-            image_array = image_array[..., np.newaxis]
+            #image_array = image_array[..., np.newaxis]
+            image_array = image_array[np.newaxis, ...]
 
         #image_array = image_array.transpose((3, 2, 0, 1))
         #label_array = label_array.transpose((2, 0, 1))
