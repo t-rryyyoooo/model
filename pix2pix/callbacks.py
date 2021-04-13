067e02c16dfeba597ec1b9daab131db4aaa9bc8c
zerebom <ice.choco.pudding.kokoro@gmail.comm>
2021-04-13 03:57:55 +0000

    R

--- pix2pix/callbacks.py
+++ pix2pix/callbacks.py
@@ -2,6 +2,9 @@ import cloudpickle
 from pathlib import Path
 import cv2
 import numpy
+from torch.utils.data import DataLoader
+from .transform import Pix2PixTransform 
+from torch.utils import data
 
 class LatestModelCheckpoint(object):
     def __init__(self, save_directory, save_name="latest.pkl"):
@@ -31,23 +34,31 @@ class BestModelCheckpoint(object):
                 cloudpickle.dump(model, f)
 
 class SavePredImages(object):
-    def __init__(self, save_directory: str, input_image_list: list, num_columns=5, save_ext="jpg"):
+    def __init__(self, save_directory: str, dataset_path,, criteria, phase="test", transform, num_columns=5, save_ext="jpg"):
         """ Predict fed images and save them in num_columns * (len(input_image_list) // num_colum) arrangement.
 
         Parameters: 
             save_directory (str)    -- Save predicted images to this.
-            input_image_list (list) -- Images fed to model.
+            dataset (data.Dataset)  -- Dataset path which has data per patient in case_xx. 
             num_columns (int)       -- The number of images lined up horizontally.
             save_ext (str)          -- Extension for saving. npy or extensions available in cv2.
         """
 
         self.save_directory   = Path(save_directory)
+
+        dataset = Pix2PixDataset(
+                    dataset_path = input_image_list
         self.input_image_list = input_image_list
+
 
     def __call__(self, pred, model, epoch):
         pred_list = []
         temp_pred_list = []
+        input_list = []
+        temp_input_list = []
+
         for i, input_image in enumerate(input_image_list):
+            input__array = input.to("cpu".detach().numpy().astype(np.float)
             pred_array = model(input_image).to("cpu").detach().numpy().astype(np.float)
             pred_array = np.squeeze(pred_array)
 
@@ -56,15 +67,24 @@ class SavePredImages(object):
                 temp_pred_list = []
                 temp_pred_list.append(pred_array)
 
+                input_list.append(temp_input_list)
+                temp_input_list = []
+                temp_input_list.append(input_array)
+
             else:
                 temp_pred_list.append(pred_array)
+                temp_input_list.append(input_array)
 
-        save_image = np.concatenate(pred_list)
+        save_pred = np.concatenate(pred_list)
+        save_img  = np.concatenate(input_list)
 
-        save_path = save_directory / "epoch_{:3d}_loss_{:.3f}.{}".format(epoch, pred, self.save_ext)
+        save_pred_path = save_directory / "pred_epoch_{:3d}_loss_{:.3f}.{}".format(epoch, pred, self.save_ext)
+        save_img_path  = save_directory / "input_epoch_{:3d}_loss_{:.3f}.{}".format(epoch, pred, self.save_ext)
 
         if self.save_ext == "npy":
-            np.save(str(save_path), save_image)
+            np.save(str(save_pred_path), save_pred)
+            np.save(str(save_img_path), save_img)
 
         else:
-            cv2.imwrite(str(save_path), save_image)
+            cv2.imwrite(str(save_pred_path), save_pred)
+            cv2.imwrite(str(save_img_path), save_img)
